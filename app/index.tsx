669e3039429d77ebd1abf0c91de4fb593f237ef5
Marcos Medeiros <srmarcoserick1@gmail.com>
2025-05-10 22:24:16 -0300

    arrumando a rota de navegação

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,17 @@
 
-import { StyleSheet, View, Text } from 'react-native';
+import { useRouter } from 'expo-router';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 
 
 
 export default function HomeScreen() {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>Home Screen</Text>
+      <TouchableOpacity onPress={() => router.push('/telaDeCadastro/index')}>
+        <Text style={styles.texto}>Tela de Cadastro</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,11 +21,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#000', 
+    backgroundColor: '#000',
   },
 
   texto: {
     color: '#fff',
     fontSize: 20,
+    textAlign: 'center',
   },
+
 });
